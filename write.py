c05d5635925b8375eee93208636105c6cd431a75
handamn <kupretzzz14@gmail.com>
2023-12-14 15:42:19 +0700

    Rev4 Write script

--- write.py
+++ write.py
@@ -1,34 +1,40 @@
 import pandas as pd
-import time
 import random
+import time
 
 #path
 path = "/home/engser/YOLO/yolov5_research2/"
 
 #nama file jembatan
-filename_tulis_csv = "baca_file_ini.csv"
+filename_tulis_csv ="data_terima.csv"
 
 #path file jembatan
 filename_tulis = path + filename_tulis_csv
 
 #nama file transform
-filename_read_transform_csv = "transform_data5.csv"
+filename_read_transform_csv ="transform_data4.csv"
 
 #path file transform
 filename_read_transform = path + filename_read_transform_csv
 
-# Read data file transform
-df_read = pd.read_csv(filename_read_transform_csv)
 
-# Write data to file jembatan without header
-for x in range(len(df_read)):
-    random_item = df_read.iloc[x].values
+#read data file transform
+df_read = pd.read_csv(filename_read_transform_csv)
+df_read_list = df_read.values.tolist()
+
+#read data file jembatan
+ef = pd.read_csv(filename_tulis)
+
+
+
+for x in range(1000000):
+    random_item = random.choice(df_read_list)
     ff = pd.DataFrame([x])
-    hf = pd.DataFrame([x * random.randrange(100)])
+    hf = pd.DataFrame([x*random.randrange(100)])
     ef = pd.DataFrame([random_item])
-    gf = pd.concat([ff, hf, ef], axis=1, join='inner')
-    gf.to_csv(filename_tulis, index=False, header=False)
+    gf = pd.concat([ff,hf,ef],axis=1, join='inner')
+    gf.to_csv(filename_tulis,index=False, header=False)
 
     print(gf)
 
-    time.sleep(30)
+    time.sleep(20)
